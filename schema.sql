0cdfd49eb469c6deb92f8cb1d48270fe62f00588
Emma Denton <emmad@192-168-1-200.tpgi.com.au>
2023-11-13 21:31:02 +1100

    added functions to handle adding role, department and employee. Added function to handle updating employee.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-USE books_db;
+USE employeeCMS_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -15,18 +15,7 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name varchar(30) NOT NULL,
-  last_name VARCHAR(30)
+  last_name VARCHAR(30),
   role_id INT,
   manager_id INT
-);
-
-
-CREATE TABLE favorite_books (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL,
-  in_stock BOOLEAN,
-  book_price INT,
-  FOREIGN KEY (book_price)
-  REFERENCES book_prices(id)
-  ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const PORT = process.env.PORT || 3000;
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL Username
     user: 'root',
-    // TODO: Add MySQL Password
     password: '',
-    database: 'books_db'
+    database: 'employeeCMS_db'
   }
 );
 
@@ -20,7 +18,7 @@ db.connect((err) => {
     console.error('Error connecting to the database:', err);
     return;
   }
-  console.log('Connected to the books_db database.');
+  console.log('Connected to the employeeCMS_db database.');
 });
 
 
@@ -65,110 +63,245 @@ function init() {
 }
 
 function viewAllEmployees() {
+  const query = 'SELECT * FROM employee';
 
-}
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving Employees:', err);
+      return;
+    }
 
-function addEmployee() {
-  inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'employeeFirstname',
-      message: "What is the employee's first name?",
-    },
-    {
-      type: 'input',
-      name: 'employeeLastname',
-      message: "What is the employee's last name?",
-    },
-    {
-      type: 'list',
-      name: 'employeeRole',
-      message: "What is this employee's role?",
-      choices: ['Role'],
-    },
-    {
-      type: 'list',
-      name: 'employeeManager',
-      message: "Who is this employee's manager?",
-      choices: ['Manager'],
-    },
-  ])
-  .then((answer) => {
-  
-    console.log('Employee added successfully.');
-    init();
-  });
-}
-
-function updateEmployeeRole() {
-  inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'employeeName',
-      message: "Which employee's role do you want to update?",
-      choices: ['Employee'],
-    },
-    {
-      type: 'list',
-      name: 'newRole',
-      message: "Which role do you want to assign to the selected employee?",
-      choices: ['Role']
-    },
-  ])
-  .then((answer) => {
-    const employeeName = answer.employeeName;
-    const newRole = answer.newRole;
-
-    console.log(`Role updated successfully for ${employeeName}`);
-    init();
-  });
-}
-
-function viewAllRoles() {
-
-}
-
-function addRole() {
-  inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'roleName',
-      message: 'What is the name of the role?',
-    },
-    {
-      type: 'input',
-      name: 'roleSalary',
-      message: 'What is the salary of the role?',
-    },
-  ])
-  .then((answer) => {
-
-    console.log(`Role "${answer.roleName}" added successfully.`);
+    console.log('All Employees:');
+    for (const employee of results) {
+      console.log(`ID: ${employee.id}, First Name: ${employee.first_name}, Last Name: ${employee.last_name}`);
+    }
     init();
   });
 }
 
 function viewAllDepartments() {
+  const query = 'SELECT * FROM department';
 
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving Departments:', err);
+      return;
+    }
+
+    console.log('All Departments:');
+    for (const department of results) {
+      console.log(`ID: ${department.id}, Department Name: ${department.department_name}`);
+    }
+    init();
+  });
 }
+
+function addEmployee() {
+  const rolesQuery = 'SELECT id, title FROM role';
+  const employeesQuery = 'SELECT first_name, id FROM employee';
+
+  db.query(rolesQuery, (err, roles, employees) => {
+    if (err) {
+      console.error('Error retrieving roles:', err);
+      return;
+    }
+
+    const roleChoices = roles.map((role) => ({ name: role.title, value: role.id }));
+    const managerChoices = employees.map((employee) => ({ name: employee.first_name, value: employee.id }));
+
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'employeeFirstname',
+          message: "What is the employee's first name?",
+        },
+        {
+          type: 'input',
+          name: 'employeeLastname',
+          message: "What is the employee's last name?",
+        },
+        {
+          type: 'list',
+          name: 'employeeRole',
+          message: "What is this employee's role?",
+          choices: roleChoices,
+        },
+        {
+          type: 'list',
+          name: 'employeeManager',
+          message: "What is this employee's manager?",
+          choices: managerChoices,
+        },
+      ])
+      .then((answer) => {
+        const firstName = answer.employeeFirstname;
+        const lastName = answer.employeeLastname;
+        const roleId = answer.employeeRole;
+        const managerId = answer.employeeManager;
+
+
+        const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+
+        db.query(query, [firstName, lastName, roleId, managerId], (err, result) => {
+          if (err) {
+            console.error('Error adding employee:', err);
+            return;
+          }
+
+          console.log(`Employee "${firstName} ${lastName}" added successfully.`);
+          init();
+        });
+      });
+  });
+}
+
+function updateEmployeeRole() {
+  const employeesQuery = 'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee';
+  db.query(employeesQuery, (err, employees) => {
+    if (err) {
+      console.error('Error retrieving employees:', err);
+      return;
+    }
+
+    const rolesQuery = 'SELECT id, title FROM role';
+    db.query(rolesQuery, (err, roles) => {
+      if (err) {
+        console.error('Error retrieving roles:', err);
+        return;
+      }
+
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'employeeName',
+            message: "Which employee's role do you want to update?",
+            choices: employees.map((employee) => employee.name),
+          },
+          {
+            type: 'list',
+            name: 'newRoleId',
+            message: "Which role do you want to assign to the selected employee?",
+            choices: roles.map((role) => ({
+              name: role.title,
+              value: role.id,
+            })),
+          },
+        ])
+        .then((answer) => {
+          const selectedEmployeeName = answer.employeeName;
+          const selectedEmployee = employees.find((employee) => employee.name === selectedEmployeeName);
+          const newRoleId = answer.newRoleId;
+
+          const updateQuery = 'UPDATE employee SET role_id = ? WHERE id = ?';
+
+          db.query(updateQuery, [newRoleId, selectedEmployee.id], (err, result) => {
+            if (err) {
+              console.error('Error updating employee role:', err);
+              return;
+            }
+
+            console.log(`Role updated successfully for ${selectedEmployeeName}`);
+            init();
+          });
+        });
+    });
+  });
+}
+
+function viewAllRoles() {
+  const query = 'SELECT * FROM role';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error retrieving roles:', err);
+      return;
+    }
+
+    console.log('All Roles:');
+    for (const role of results) {
+      console.log(`ID: ${role.id}, Title: ${role.title}, Salary: ${role.salary}, Department ID: ${role.department_id}`);
+    }
+    init();
+  });
+}
+
+function addRole() {
+  const departmentQuery = 'SELECT id, department_name FROM department';
+
+  db.query(departmentQuery, (err, department) => {
+    if (err) {
+      console.error('Error retrieving department:', err);
+      return;
+    }
+
+    const departmentChoices = department.map((department) => ({ name: department.department_name, value: department.id }));
+
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'roleName',
+        message: 'What is the name of the role?',
+      },
+      {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'What is the salary of the role?',
+      },
+  
+      {
+        type: 'list',
+        name: 'roleDepartment',
+        message: 'What department is this role in?',
+        choices: departmentChoices,
+      },
+    ])
+    .then((answer) => {
+      const roleName = answer.roleName;
+      const roleSalary = parseFloat(answer.roleSalary);
+  
+  
+      const query = 'INSERT INTO role (title, salary) VALUES (?, ?)';
+  
+      db.query(query, [roleName, roleSalary], (err, result) => {
+        if (err) {
+          console.error('Error adding role:', err);
+          return;
+        }
+  
+        console.log(`Role "${roleName}" added successfully with a salary of ${roleSalary}.`);
+        init();
+        });
+      });
+  });
+}
+
 
 function addDepartment() {
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'departmentName',
-      message: 'What is the name of the department?',
-    },
-  ])
-  .then((answer) => {
- 
-    console.log(`Department "${answer.departmentName}" added successfully.`);
-    init();
-  });
+    .prompt([
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'What is the name of the department?',
+      },
+    ])
+    .then((answer) => {
+      const departmentName = answer.departmentName;
+      const query = 'INSERT INTO department (department_name) VALUES (?)';
+
+      db.query(query, [departmentName], (err, result) => {
+        if (err) {
+          console.error('Error adding department:', err);
+          return;
+        }
+
+        console.log(`Department "${departmentName}" added successfully.`);
+        init();
+      });
+    });
 }
 
 init();
